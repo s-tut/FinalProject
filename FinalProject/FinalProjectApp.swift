638fe1d41f8e6ec1e4ba92d20a60d34b1cfad3f5
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-26 11:00:59 -0700

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
