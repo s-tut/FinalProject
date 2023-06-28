e6dbed31378c110f9e07069d7a05beae5f7367b9
nil <nilegbo@gmail.com>
2023-06-28 15:17:11 -0700

    worked on home screen

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -14,43 +14,57 @@ struct ContentView: View {
                 Text("Dream Catcher")
                     .font(.title)
                     .fontWeight(.bold)
-                Button("journal") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                .padding()
-                .frame(width: nil)
-                .toolbar {
-                        ToolbarItemGroup(placement: .status) {
-                            NavigationLink(destination: Journal()) {
-                                Text("Journal")
-                            
-                            }
-                            NavigationLink(destination: Resources()) {
-                                Text("Resources")
-                            
-                            }
-                            NavigationLink(destination: Settings()) {
-                                Text("Settings")
-                            
-                            }
-                            NavigationLink(destination: About()) {
-                                Text("About")
-                            
-                            }
-                        
-
-                        }
+                HStack {
+                    NavigationLink(destination: Journal()) {
+                       Image("journal")
                         
                     }
+                    
+                    NavigationLink(destination: Resources()) {
+                        Text("Resources")
+                        
+                    }
+                }
+                HStack {    NavigationLink(destination: Settings()) {
+                    Text("Settings")
+                    
+                }
+                NavigationLink(destination: About()) {
+                    Text("About")
+                    
+                }
+                    
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .status) {
+                   
+                        NavigationLink(destination: Settings()) {
+                            Text("Settings")
+                            
+                        }
+                        NavigationLink(destination: About()) {
+                            Text("About")
+                            
+                        }
+                    }
+                    
+                    //                 }
+                }
+                //
             }
-            
-            .navigationTitle("Home")
-            .navigationBarTitleDisplayMode(.inline)
-            
         }
+        .navigationTitle("Home")
+                   .navigationBarTitleDisplayMode(.inline)
     }
-        
 }
+            
+//            .navigationTitle("Home")
+//            .navigationBarTitleDisplayMode(.inline)
+//
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
